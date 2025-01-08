df954629bb0cd86dc0abb6a530be819b9bc6341d
drkchu <derekdbchu@gmail.com>
2025-01-08 11:16:43 -0800

    feat: update cron to prevent inactivity

--- src/database_dont_die.js
+++ src/database_dont_die.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-cron.schedule('11 23 * * *', async () => {
+cron.schedule('*/14 * * * *', async () => { // Updates every 14 minutes so that it doesn't spin down, kinda ugly
     try {
         const guestUser = await prisma.user.findUnique({
             where: { username: "guest" }
